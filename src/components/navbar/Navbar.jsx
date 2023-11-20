a25ce6ba90ed41f4a0bd4255804671d6507d94cc
Nurul Amin <namin2750@gmail.com>
2023-11-20 23:20:48 +0600

    logo link

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom";
 import "./navbar.css";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">Booking App</span>
+        <span className="logo">
+          <Link className="logoLink" to="/">
+            Booking App
+          </Link>
+        </span>
         <div className="navItems">
           <button className="navbtn">Register</button>
           <button className="navbtn">login</button>
