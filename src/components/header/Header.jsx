ccc8b0277365aa3de26450acd8220a471f407eb9
Nurul Amin <namin2750@gmail.com>
2023-11-20 18:47:49 +0600

    header search

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./header.css";
 import {
   faBed,
+  faCalendarDays,
   faCar,
+  faPerson,
   faPlane,
   faTaxi,
 } from "@fortawesome/free-solid-svg-icons";
@@ -35,10 +37,31 @@ const Header = () => {
         </div>
         <h1 className="headerTitle">A lifetime of discounts? It's Genius.</h1>
         <p className="headerDesc">
-          Get rewarded for your travel-unlock instany=t saving 100% or more with
-          a free Booking App account
+          Get rewarded for your travel-unlock instant of saving 100% or more
+          with a free Booking App account.
         </p>
         <button className="headerBtn">Sign In / Register</button>
+        <div className="headerSearch">
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faBed} className="headerIcon" />
+            <input
+              type="text"
+              className="headerSearchInput"
+              placeholder="Where are you going?"
+            />
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
+            <span className="headerSearchText">date to date</span>
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faPerson} className="headerIcon" />
+            <span className="headerSearchText">2 adults 2 children 1 room</span>
+          </div>
+          <div className="headerSearchItem">
+            <button className="headerBtn">Search</button>
+          </div>
+        </div>
       </div>
     </div>
   );
