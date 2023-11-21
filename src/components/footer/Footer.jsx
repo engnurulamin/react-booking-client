e677208c9be7094e6cfbc8b8931a44350207a4b0
Nurul Amin <namin2750@gmail.com>
2023-11-21 20:52:30 +0600

    footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,53 @@
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="fLists">
+        <ul className="fList">
+          <li className="fListItem">Countries</li>
+          <li className="fListItem">Region</li>
+          <li className="fListItem">Cities</li>
+          <li className="fListItem">Districts</li>
+          <li className="fListItem">Airports</li>
+          <li className="fListItem">Hotels</li>
+        </ul>
+        <ul className="fList">
+          <li className="fListItem">Countries</li>
+          <li className="fListItem">Region</li>
+          <li className="fListItem">Cities</li>
+          <li className="fListItem">Districts</li>
+          <li className="fListItem">Airports</li>
+          <li className="fListItem">Hotels</li>
+        </ul>
+        <ul className="fList">
+          <li className="fListItem">Countries</li>
+          <li className="fListItem">Region</li>
+          <li className="fListItem">Cities</li>
+          <li className="fListItem">Districts</li>
+          <li className="fListItem">Airports</li>
+          <li className="fListItem">Hotels</li>
+        </ul>
+        <ul className="fList">
+          <li className="fListItem">Countries</li>
+          <li className="fListItem">Region</li>
+          <li className="fListItem">Cities</li>
+          <li className="fListItem">Districts</li>
+          <li className="fListItem">Airports</li>
+          <li className="fListItem">Hotels</li>
+        </ul>
+        <ul className="fList">
+          <li className="fListItem">Homes</li>
+          <li className="fListItem">Apartments</li>
+          <li className="fListItem">Resorts</li>
+          <li className="fListItem">Villas</li>
+          <li className="fListItem">Hostels</li>
+          <li className="fListItem">B&Bs</li>
+        </ul>
+      </div>
+      <div className="fText">&copy; 2023 Booking App. All rights reserved.</div>
+    </div>
+  );
+};
+
+export default Footer;
