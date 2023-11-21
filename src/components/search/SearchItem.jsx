28cc0cf6fd30f83fe64c8c201af26317f8bdf108
Nurul Amin <namin2750@gmail.com>
2023-11-21 23:50:55 +0600

    search item details part

---
+++ src/components/search/SearchItem.jsx
@@ -0,0 +1,208 @@
+import "./searchItem.css";
+
+const SearchItem = () => {
+  return (
+    <>
+      <div className="searchItem">
+        <img
+          className="siImg"
+          src="https://source.unsplash.com/k9Zeq6EH_bk"
+          alt=""
+        />
+        <div className="desc">
+          <h1 className="title"> Tower Street Aprtment</h1>
+          <span className="distance">500m from center</span>
+          <span className="taxiOp">Free airport taxi</span>
+          <span className="subtitle">
+            Studio aprtment with air conditioning
+          </span>
+          <span className="features">
+            Entire Studio . 1 bathroom . 21m² 1 full bed
+          </span>
+          <span className="cancelOp">Free cancellation</span>
+          <span className="cancelOpsub">
+            You can cancel later, so lock in this great price today!
+          </span>
+        </div>
+        <div className="details">
+          <div className="rating">
+            <span>Excellent</span>
+            <button>8.9</button>
+          </div>
+          <div className="detailText">
+            <span className="price">$2000</span>
+            <span className="textOp">Includes taxes And fees</span>
+            <button className="checkBtn">See Avialability</button>
+          </div>
+        </div>
+      </div>
+      <div className="searchItem">
+        <img
+          className="siImg"
+          src="https://source.unsplash.com/k9Zeq6EH_bk"
+          alt=""
+        />
+        <div className="desc">
+          <h1 className="title"> Tower Street Aprtment</h1>
+          <span className="distance">500m from center</span>
+          <span className="taxiOp">Free airport taxi</span>
+          <span className="subtitle">
+            Studio aprtment with air conditioning
+          </span>
+          <span className="features">
+            Entire Studio . 1 bathroom . 21m² 1 full bed
+          </span>
+          <span className="cancelOp">Free cancellation</span>
+          <span className="cancelOpsub">
+            You can cancel later, so lock in this great price today!
+          </span>
+        </div>
+        <div className="details">
+          <div className="rating">
+            <span>Excellent</span>
+            <button>8.9</button>
+          </div>
+          <div className="detailText">
+            <span className="price">$2000</span>
+            <span className="textOp">Includes taxes And fees</span>
+            <button className="checkBtn">See Avialability</button>
+          </div>
+        </div>
+      </div>
+      <div className="searchItem">
+        <img
+          className="siImg"
+          src="https://source.unsplash.com/k9Zeq6EH_bk"
+          alt=""
+        />
+        <div className="desc">
+          <h1 className="title"> Tower Street Aprtment</h1>
+          <span className="distance">500m from center</span>
+          <span className="taxiOp">Free airport taxi</span>
+          <span className="subtitle">
+            Studio aprtment with air conditioning
+          </span>
+          <span className="features">
+            Entire Studio . 1 bathroom . 21m² 1 full bed
+          </span>
+          <span className="cancelOp">Free cancellation</span>
+          <span className="cancelOpsub">
+            You can cancel later, so lock in this great price today!
+          </span>
+        </div>
+        <div className="details">
+          <div className="rating">
+            <span>Excellent</span>
+            <button>8.9</button>
+          </div>
+          <div className="detailText">
+            <span className="price">$2000</span>
+            <span className="textOp">Includes taxes And fees</span>
+            <button className="checkBtn">See Avialability</button>
+          </div>
+        </div>
+      </div>
+      <div className="searchItem">
+        <img
+          className="siImg"
+          src="https://source.unsplash.com/k9Zeq6EH_bk"
+          alt=""
+        />
+        <div className="desc">
+          <h1 className="title"> Tower Street Aprtment</h1>
+          <span className="distance">500m from center</span>
+          <span className="taxiOp">Free airport taxi</span>
+          <span className="subtitle">
+            Studio aprtment with air conditioning
+          </span>
+          <span className="features">
+            Entire Studio . 1 bathroom . 21m² 1 full bed
+          </span>
+          <span className="cancelOp">Free cancellation</span>
+          <span className="cancelOpsub">
+            You can cancel later, so lock in this great price today!
+          </span>
+        </div>
+        <div className="details">
+          <div className="rating">
+            <span>Excellent</span>
+            <button>8.9</button>
+          </div>
+          <div className="detailText">
+            <span className="price">$2000</span>
+            <span className="textOp">Includes taxes And fees</span>
+            <button className="checkBtn">See Avialability</button>
+          </div>
+        </div>
+      </div>
+      <div className="searchItem">
+        <img
+          className="siImg"
+          src="https://source.unsplash.com/k9Zeq6EH_bk"
+          alt=""
+        />
+        <div className="desc">
+          <h1 className="title"> Tower Street Aprtment</h1>
+          <span className="distance">500m from center</span>
+          <span className="taxiOp">Free airport taxi</span>
+          <span className="subtitle">
+            Studio aprtment with air conditioning
+          </span>
+          <span className="features">
+            Entire Studio . 1 bathroom . 21m² 1 full bed
+          </span>
+          <span className="cancelOp">Free cancellation</span>
+          <span className="cancelOpsub">
+            You can cancel later, so lock in this great price today!
+          </span>
+        </div>
+        <div className="details">
+          <div className="rating">
+            <span>Excellent</span>
+            <button>8.9</button>
+          </div>
+          <div className="detailText">
+            <span className="price">$2000</span>
+            <span className="textOp">Includes taxes And fees</span>
+            <button className="checkBtn">See Avialability</button>
+          </div>
+        </div>
+      </div>
+      <div className="searchItem">
+        <img
+          className="siImg"
+          src="https://source.unsplash.com/k9Zeq6EH_bk"
+          alt=""
+        />
+        <div className="desc">
+          <h1 className="title"> Tower Street Aprtment</h1>
+          <span className="distance">500m from center</span>
+          <span className="taxiOp">Free airport taxi</span>
+          <span className="subtitle">
+            Studio aprtment with air conditioning
+          </span>
+          <span className="features">
+            Entire Studio . 1 bathroom . 21m² 1 full bed
+          </span>
+          <span className="cancelOp">Free cancellation</span>
+          <span className="cancelOpsub">
+            You can cancel later, so lock in this great price today!
+          </span>
+        </div>
+        <div className="details">
+          <div className="rating">
+            <span>Excellent</span>
+            <button>8.9</button>
+          </div>
+          <div className="detailText">
+            <span className="price">$2000</span>
+            <span className="textOp">Includes taxes And fees</span>
+            <button className="checkBtn">See Avialability</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SearchItem;
