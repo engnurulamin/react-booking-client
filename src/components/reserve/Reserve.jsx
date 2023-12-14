f1acf4c481525027e440e086914670e570ca6e34
Nurul Amin <namin2750@gmail.com>
2023-12-14 23:34:56 +0600

    reserve modal markup

--- src/components/reserve/Reserve.jsx
+++ src/components/reserve/Reserve.jsx
@@ -1,7 +1,20 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./reserve.css";
+import { faCircleXmark } from "@fortawesome/free-regular-svg-icons";
 
 const Reserve = () => {
-  return <div>Reserve</div>;
+  return (
+    <div className="reserve">
+      <div className="reserve-container">
+        <FontAwesomeIcon
+          icon={faCircleXmark}
+          className="re-close"
+          onClick={() => setOpen(false)}
+        />
+        <span>Select Your rooms:</span>
+      </div>
+    </div>
+  );
 };
 
 export default Reserve;
